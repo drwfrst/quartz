eda340f3070073e7aa9b5bc9c8017294fa8f8695
drwfrst <darkwhite.forest@gmail.com>
2025-06-26 23:23:29 +0800

    My first Quartz site

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "fromm.jwgo.xyz",
+    baseUrl: "drwfrst.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
