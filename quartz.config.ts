e5d8ea8e1b0b9153d13980d95f0714f2c5d48c46
drwfrst <darkwhite.forest@gmail.com>
2025-07-01 21:25:17 +0800

    Quartz sync: Jul 1, 2025, 9:25 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "JwgoSelect",
+    pageTitle: "Jwgo",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
